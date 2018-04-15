241b48264132abf554977475e5e0192abcb30d50
AleksMala <o.malaszuk@gmail.com>
2018-04-15 12:20:32 +0200

    First commit

---
+++ src/main/java/com/projects/javaprojects/JavaProjectsApplication.java
@@ -0,0 +1,12 @@
+package com.projects.javaprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectsApplication.class, args);
+	}
+}
